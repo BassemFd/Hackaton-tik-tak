58e4d0e7b0002f982372802dc22df55862d384bb
Frederic PRINCE <fredericprince@MacBook-Pro.concorde-pereire.loc>
2021-01-28 14:37:58 +0100

    users mango connected

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,77 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+
+var userModel = require('../models/users');
+
+//! Route sign-up
+
+router.post('/sign-up', async function(req,res,next){
+
+  var searchUser = await userModel.findOne({
+    email: req.body.emailFromFront
+  })
+  
+  if(!searchUser){
+    var newUser = new userModel({
+      name: req.body.nameFromFront,
+      firstname: req.body.firstnameFromFront,
+      email: req.body.emailFromFront,
+      password: req.body.passwordFromFront,
+    })
+  
+    var newUserSave = await newUser.save();
+    
+    req.session.user = {
+      name: newUserSave.username,
+      id: newUserSave._id,
+    }
+  
+    console.log(req.session.user)
+  
+    res.redirect('/homepage')
+  } else {
+    res.redirect('/')
+  }
+  
 });
+
+
+//! Route sign-in
+
+router.post('/sign-in', async function(req,res,next){
+
+  var searchUser = await userModel.findOne({
+    email: req.body.emailFromFront,
+    password: req.body.passwordFromFront
+  })
+
+  if(searchUser!= null){
+    req.session.user = {
+      name: searchUser.username,
+      id: searchUser._id
+    }
+    res.redirect('/homepage')
+  } else {
+    res.render('/')
+  }
+
+  
+});
+
+//! Route log-out
+
+router.get('/logout', function(req,res,next){
+
+  req.session.user = null;
+
+  res.redirect('/')
+});
+
+
 
 module.exports = router;
